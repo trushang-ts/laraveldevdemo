4844dd6ff6114d162be293e50e06c5c6f0008873
trushang.suthar <trushang@technostacks.com>
2019-12-26 16:55:48 +0530

    Ajax Login

--- app/Http/Controllers/admin/Auth/LoginController.php
+++ app/Http/Controllers/admin/Auth/LoginController.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\admin\Auth;
 
+use Auth;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -35,22 +37,34 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
-        $this->middleware('guest:admin')->except('logout');
+        // $this->middleware('guest')->except('logout');
+        // $this->middleware('guest:admin')->except('logout');
     }
 
     public function adminLogin(Request $request)
     {
+        
         $this->validate($request, [
             'email'   => 'required|email',
             'password' => 'required|min:6'
-        ]);
+        ]); 
         
-        
-        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
-
-            return redirect()->intended('/admin');
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], true)) {
+            $msg = array(
+                'code' => 200,
+				'status'  => 'success',
+                'message' => 'Login Successful',
+                'intended' => $this->redirectTo
+			);
+			return response()->json($msg);
+        }else{
+            $msg = array(
+                'code' => 400,
+				'status'  => 'error',
+				'message' => 'Login Un Successful'
+			);
+			return response()->json($msg);
         }
-        return back()->withInput($request->only('email', 'remember'));
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/abc', function () {
-    return view('/admin/template');
-});
 
+//Auth::routes();
 
-
-Route::group(['prefix' => 'admin', 'as'=>'admin.','namespace' => 'admin'], function () {
+Route::group(['prefix' => 'admin', 'as'=>'admin.','namespace' => 'admin', 'middleware' => 'admin'], function () {
     
+    // \DB::listen(function($sql) {
+    //     \Log::info($sql->sql);
+    //     \Log::info($sql->bindings);
+    //     \Log::info($sql->time);
+    // });
+
+    Route::post('/doLogin',"Auth\LoginController@adminLogin");
+    Route::get('/dashboard',"EmployeeController@dashboard");
+
+    Route::post('login', [ 'as' => 'login', 'uses' => 'LoginController@do']);
     
     Route::get('/', function () {
         return view('/admin/page/users');
+    });
+
+    Route::get('/login', function () {
+        return view('/admin/page/login');
     });
 
     Route::get('/users', function () {
@@ -27,5 +38,6 @@ Route::group(['prefix' => 'admin', 'as'=>'admin.','namespace' => 'admin'], funct
 
     Route::get('/employee/index',"EmployeeController@index");
     Route::post('/employee/storeEmployee',"EmployeeController@storeEmployee");
+    
 
 });
